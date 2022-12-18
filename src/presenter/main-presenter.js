0ce1ca396fdd24e11a6d512af6b677970f8df5cb
r1zyh <kirill.ananiev2012@yandex.ru>
2022-12-18 23:38:55 +0300

    Удалит ненужное

--- src/presenter/main-presenter.js
+++
@@ -1,8 +0,0 @@
-import FilmsPresenter from '../presenter/films-presenter.js';
-import FilmsModel from '../model/film-model.js';
-
-const siteMain = document.querySelector('.main');
-const filmModel = new FilmsModel();
-
-export const filmsPresenter = new FilmsPresenter({ filmsContainer: siteMain, filmModel });
-
