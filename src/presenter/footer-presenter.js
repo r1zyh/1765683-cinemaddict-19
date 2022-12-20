7ebbac9bee066052aaa7826674604dc8c4b19ab6
r1zyh <kirill.ananiev2012@yandex.ru>
2022-12-21 01:18:03 +0300

    Подправит footer-presenter

--- src/presenter/footer-presenter.js
+++ src/presenter/footer-presenter.js
@@ -2,11 +2,11 @@ import { render } from '../render';
 import FooterStatisticsView from '../view/footer-statistics-view.js';
 
 export default class FooterPresenter {
+  siteFooter = document.querySelector('.footer');
+  footerComponent = new FooterStatisticsView();
+
   init() {
-    const siteFooter = document.querySelector('.footer');
 
-    const footerComponent = new FooterStatisticsView();
-
-    render(footerComponent, siteFooter);
+    render(this.footerComponent, this.siteFooter);
   }
 }
