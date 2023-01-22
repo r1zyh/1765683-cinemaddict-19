86a547dbe28cb4d81fa7a1700037fe74e18b76d6
r1zyh <kirill.ananiev2012@yandex.ru>
2023-01-22 16:00:35 +0300

    Исправит разметку

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -2,7 +2,7 @@ import { FilterType } from '../mock/const.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 function createFilterItemTemplate(filter, count, isChecked) {
-  return `<a href="#${filter}" class="main-navigation__item
+  return `<a href="${filter}" class="main-navigation__item
      ${isChecked ? 'main-navigation__item--active' : ''}">
       ${FilterType[filter]}
       ${filter === 'all' ? '' : `<span class="main-navigation__item-count">${count}</span>`}
