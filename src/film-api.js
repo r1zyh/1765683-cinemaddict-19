acd33d57beebba4768ea63df9b0d40fb9b708b8a
r1zyh <kirill.ananiev2012@yandex.ru>
2023-02-13 00:07:07 +0300

    Добавит api

---
+++ src/film-api.js
@@ -0,0 +1,54 @@
+import ApiService from './framework/api-service.js';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class FilmsApiService extends ApiService {
+  get films() {
+    return this._load({url: 'movies'})
+      .then(ApiService.parseResponse);
+  }
+
+  async updateFilm(film) {
+    const response = await this._load({
+      url: `movies/${film.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(this.#adaptToServer(film)),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    return await ApiService.parseResponse(response);
+  }
+
+  #adaptToServer(film) {
+    const adaptedFilm = {
+      'id': film.id,
+      'comments': film.comments.map((comment) => comment.id),
+      'film_info': {
+        ...film.filmInfo,
+        'age_rating': film.filmInfo.ageRating,
+        'alternative_title': film.filmInfo.alternativeTitle,
+        'total_rating': film.filmInfo.ageRating,
+        'release': {
+          'date': film.filmInfo.release.date,
+          'release_country': film.filmInfo.release.releaseCountry
+        }
+      },
+      'user_details': {
+        ...film.userDetails,
+        'already_watched': film.userDetails.alreadyWatched,
+        'watching_date': film.userDetails.watchingDate,
+      }
+    };
+
+    delete adaptedFilm.film_info.ageRating;
+    delete adaptedFilm.film_info.alternativeTitle;
+    delete adaptedFilm.film_info.totalRating;
+    delete adaptedFilm.user_details.alreadyWatched;
+    delete adaptedFilm.user_details.watchingDate;
+
+    return adaptedFilm;
+  }
+}
